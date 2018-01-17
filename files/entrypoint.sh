962974f527dbca4e7d319ef88e7dae8819b6d20d
David Hain <dhain@spideroak.com>
2018-01-17 13:53:38 +0000

    Add letsencrypt support

--- files/entrypoint.sh
+++ files/entrypoint.sh
@@ -5,8 +5,18 @@ set -x
 export NAGIOS_BASE_URL=`sed s./*$./. <<<${NAGIOS_BASE_URL:-/nagios/}`
 export > /etc/default/docker-envs
 
-if [ -f /etc/nginx/conf.d/nagios.conf ]; then
-    sed -i "s|{{NAGIOS_BASE_URL}}|$NAGIOS_BASE_URL|g" /etc/nginx/conf.d/nagios.conf
+if [ -f /etc/nginx/conf.d/nagios/nagios.conf ]; then
+    sed -i "s|{{NAGIOS_BASE_URL}}|$NAGIOS_BASE_URL|g" /etc/nginx/conf.d/nagios/nagios.conf
+fi
+
+if [ "$LETSENCRYPT_DOMAINS" ]; then
+    mv /etc/nginx/conf.d/default-http.conf{,.disabled}
+    mv /etc/nginx/conf.d/le-http.conf{.disabled,}
+    for domain in $LETSENCRYPT_DOMAINS; do
+        if [ -e /etc/nginx/certs/live/$domain/fullchain.pem ]; then
+            sed "s/{{DOMAIN_NAME}}/$domain/g" /etc/nginx/conf.d/le-https.conf.tmpl > /etc/nginx/conf.d/$domain.conf
+        fi
+    done
 fi
 
 if [ "$PAGERDUTY_INTEGRATION_KEY" ]; then
@@ -17,11 +27,11 @@ fi
 
 if [ "$NAGIOSADMIN_PASSWORD" ]; then
     echo "nagiosadmin:$(openssl passwd -crypt "$NAGIOSADMIN_PASSWORD")" > /etc/nginx/nagios.htpasswd
-    sed -i 's/#auth_basic/auth_basic/' /etc/nginx/conf.d/nagios.conf
+    sed -i 's/#auth_basic/auth_basic/' /etc/nginx/conf.d/nagios/nagios.conf
 fi
 
 if [ "$PROXY_ALLOW_REMOTE_USER" ]; then
-    sed -i 's/$remote_user/$http_remote_user/' /etc/nginx/conf.d/nagios.conf
+    sed -i 's/$remote_user/$http_remote_user/' /etc/nginx/conf.d/nagios/nagios.conf
     echo 'underscores_in_headers on;' > /etc/nginx/conf.d/underscores_in_headers.conf
 fi
 
---
+++ install.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+apt install -y docker.io runit
+
+if [ ! -d /etc/service/nagios ]; then
+    echo Installing nagios service runscript
+    mkdir -p /etc/service/nagios
+    cat > /etc/service/nagios/run <<EOF
+#!/bin/bash
+
+exec docker run \\
+    --name nagios \\
+    -e LETSENCRYPT_DOMAINS=example.com \\
+    -e NAGIOSADMIN_PASSWORD=superstrongpassword \\
+    -v /etc/nagios:/opt/nagios/etc:ro \\
+    -v /var/www/letsencrypt:/var/www/letsencrypt:ro \\
+    -v /etc/letsencrypt:/etc/nginx/certs:ro \\
+    -p 80:80 -p 443:443 \\
+    --rm nagios-pagerduty:latest
+EOF
+    chmod 750 /etc/service/nagios/run
+fi
+
+if crontab -l | grep -q 'certbot renew'; then
+    echo Installing certbot renew cronjob
+    crontab - <<EOF
+5 0 * * * /usr/bin/docker run --rm -v /var/www/letsencrypt:/var/www/letsencrypt -v /var/lib/letsencrypt:/var/lib/letsencrypt -v /etc/letsencrypt:/etc/letsencrypt -it certbot/certbot renew
+EOF
+fi
